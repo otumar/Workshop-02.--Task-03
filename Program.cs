cbe4aced6e0a2323df4cb107e3f6a606e547f904
«Тумар <otumar@yandex.ru>
2022-06-14 01:01:02 +0300

    Task03

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+
+Console.Write("Введите число => ");
+string paramString = Console.ReadLine();
+int Paramint = int.Parse(paramString);
+
+if (Paramint < 100) Console.Write("Третьей цифры нет");
+else
+{
+    while (Paramint > 999)
+    {
+        Paramint = Paramint / 10;
+    }
+    Paramint = Paramint % 100 % 10;
+    Console.Write($"{Paramint} - третья цифра вашего числа");
+}
